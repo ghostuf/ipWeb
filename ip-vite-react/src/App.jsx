63981adf62d92e734b00542cb3b46dc71b7765b8
ghostuf <srijan102dahal@gmail.com>
2024-01-24 19:49:01 +0545

    Added: essential elements

--- ip-vite-react/src/App.jsx
+++ ip-vite-react/src/App.jsx
@@ -7,6 +7,18 @@ export default function(){
     <div className="Main-container">
         <img src = {vectors} className = "vector-img"/>
         <img src = {geometry} className = "geometry-img"/>
+        <div className = "header">
+          Track IPs
+        </div>
+
+        <div className="main-container">
+          <div className = "input-container">
+            <input type = "text" className = "input-field"></input>
+          </div>
+          <div className = "output-terminal">
+            
+          </div>
+        </div>
     </div>
   )
 }
