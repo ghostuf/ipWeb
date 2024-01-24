69e5426ce1de1a501110b6e2cd21249c3ae23944
ghostuf <srijan102dahal@gmail.com>
2024-01-24 21:17:03 +0545

    Add and Changed: Css properties and search picture

--- ip-vite-react/src/App.jsx
+++ ip-vite-react/src/App.jsx
@@ -7,6 +7,7 @@ import vector4 from "./assets/vector4.svg"
 import vector5 from "./assets/vector5.svg"
 import vector6 from "./assets/vector6.svg"
 import vector7 from "./assets/vector7.svg"
+import search from "./assets/search.png"
 
 export default function(){
   return(
@@ -19,17 +20,22 @@ export default function(){
         <img src = {vector5} className = "vector5"/>
         <img src = {vector6} className = "vector6"/>
         <img src = {vector7} className = "vector7"/>
+      
 
-        <div className = "header">
-          Track IPs
-        </div>
-
-        <div className="main-container">
-          <div className = "input-container">
-            <input type = "text" className = "input-field"></input>
+        <div className="Content-container">
+          <div className = "header">
+            Track IPs
           </div>
-          <div className = "output-terminal">
 
+          <div className="input-terminal-container">
+            <div className = "input-container">
+              <input type = "text" className = "input-field" placeholder="Enter your ip address here..."></input>
+              <button className="input-button">
+                <img src = {search} className="search-img"/>
+              </button>
+            </div>
+            <div className = "output-terminal">
+            </div>
           </div>
         </div>
     </div>
