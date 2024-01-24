9a56fffc220e450003b08a1136b14245de8310fd
ghostuf <srijan102dahal@gmail.com>
2024-01-24 19:44:17 +0545

    Changed: some data

--- ip-vite-react/src/App.jsx
+++ ip-vite-react/src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import geometry from "./assets/geometry.svg"
+import vectors from "./assets/vector1.svg"
 
 export default function(){
   return(
     <div className="Main-container">
-      
+        <img src = {vectors} className = "vector-img"/>
+        <img src = {geometry} className = "geometry-img"/>
     </div>
   )
 }
